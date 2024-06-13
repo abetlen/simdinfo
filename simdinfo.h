60af0f4f049e5e84e68a2602462a0422d883a000
Andrei Betlen <abetlen@gmail.com>
2024-06-13 02:26:49 -0400

    Change internal fields to make header file smaller, more maintainable

--- simdinfo.h
+++ simdinfo.h
@@ -23,19 +23,34 @@
 //
 // simdinfo.h
 //
-// simdinfo is a header-only library for detecting SIMD instruction set support
-// at runtime and compile time. It can be used to build programs for specific
-// architectures like x86_64 that can take advantage of multiple instruction
-// sets depending on the machine it is running on.
+// simdinfo is a header-only library for detecting SIMD support at runtime.
 //
-// The library exposes a struct `simdinfo_t` that contains flags for various
-// SIMD instruction sets.
+// Say you have a function that already checks for SIMD support at compile time
+// 
+// ```c
+// float sum(float *a, size_t size) {
+// #if defined(__AVX__) || defined(__AVX2__)
+//   return sum_avx(a, size);
+// #else
+//   return sum_scalar(a, size);
+// #endif
+// }
+// ```
 //
-// There are two functions exposed by the library:
-// - `simdinfo_static` returns a `simdinfo_t` struct with compile-time
-// information about SIMD support.
-// - `simdinfo_runtime` returns a `simdinfo_t` struct with runtime
-// information about SIMD support.
+// You can replace the compile-time check with a runtime check:
+//
+// ```c
+// #include "simdinfo.h"
+//
+// float sum(float *a, size_t size) {
+//   simdinfo_t info = simdinfo();
+//   if (SIMDINFO_SUPPORTS(info, __AVX__) || SIMDINFO_SUPPORTS(info, __AVX2__)) {
+//     return sum_avx(a, size);
+//   } else {
+//     return sum_scalar(a, size);
+//   }
+// }
+// ```
 //
 
 #ifndef _SIMDINFO_H_
@@ -52,56 +67,29 @@
 
 typedef struct simdinfo_t {
   // x86 and x86_64
-  unsigned supports_avx;
-  unsigned supports_avx2;
-  unsigned supports_avxvnni;
-  unsigned supports_f16c;
-  unsigned supports_fma;
-  unsigned supports_avx512f;
-  unsigned supports_avx512fp16;
-  unsigned supports_avx512bf16;
-  unsigned supports_avx512vnni;
-  unsigned supports_avx512vbmi;
-  unsigned supports_avx512dq;
-  unsigned supports_sse3;
-  unsigned supports_ssse3;
+  unsigned _supports__AVX__;
+  unsigned _supports__AVX2__;
+  unsigned _supports__AVXVNNI__;
+  unsigned _supports__F16C__;
+  unsigned _supports__FMA__;
+  unsigned _supports__AVX512F__;
+  unsigned _supports__AVX512BF16__;
+  unsigned _supports__AVX512VNNI__;
+  unsigned _supports__AVX512VBMI__;
+  unsigned _supports__AVX512DQ__;
+  unsigned _supports__SSE3__;
+  unsigned _supports__SSSE3__;
 
   // arm and aarch64
-  unsigned supports_neon;
-  unsigned supports_neon_fma;
-  unsigned supports_sve;
-  unsigned supports_sve2;
-  unsigned supports_arm_feature_matmul_int8;
-  unsigned supports_arm_feature_fp16_vector_arithmetic;
+  unsigned _supports__ARM_NEON;
+  unsigned _supports__ARM_FEATURE_FMA;
+  unsigned _supports__ARM_FEATURE_MATMUL_INT8;
+  unsigned _supports__ARM_FEATURE_FP16_VECTOR_ARITHMETIC;
+  unsigned _supports__ARM_FEATURE_SVE;
+  unsigned _supports__ARM_FEATURE_SVE2;
 } simdinfo_t;
 
-#define _SIMDINFO_SUPPORTS__AVX__(info) ((info).supports_avx)
-#define _SIMDINFO_SUPPORTS__AVX2__(info) ((info).supports_avx2)
-
-#define _SIMDINFO_SUPPORTS__F16C__(info) ((info).supports_f16c)
-#define _SIMDINFO_SUPPORTS__FMA__(info) ((info).supports_fma)
-
-#define _SIMDINFO_SUPPORTS__AVXVNNI__(info) ((info).supports_avxvnni)
-
-#define _SIMDINFO_SUPPORTS__AVX512F__(info) ((info).supports_avx512f)
-#define _SIMDINFO_SUPPORTS__AVX512BF16__(info) ((info).supports_avx512bf16)
-#define _SIMDINFO_SUPPORTS__AVX512VNNI__(info) ((info).supports_avx512vnni)
-#define _SIMDINFO_SUPPORTS__AVX512VBMI__(info) ((info).supports_avx512vbmi)
-#define _SIMDINFO_SUPPORTS__AVX512DQ__(info) ((info).supports_avx512dq)
-
-#define _SIMDINFO_SUPPORTS__SSE3(info) ((info).supports_sse3)
-#define _SIMDINFO_SUPPORTS__SSSE3(info) ((info).supports_ssse3)
-
-#define _SIMDINFO_SUPPORTS__ARM_NEON(info) ((info).supports_neon)
-#define _SIMDINFO_SUPPORTS__ARM_FEATURE_FMA(info) ((info).supports_neon_fma)
-#define _SIMDINFO_SUPPORTS__ARM_FEATURE_SVE(info) ((info).supports_sve)
-#define _SIMDINFO_SUPPORTS__ARM_FEATURE_SVE2(info) ((info).supports_sve2)
-#define _SIMDINFO_SUPPORTS__ARM_FEATURE_MATMUL_INT8(info)                      \
-  ((info).supports_arm_feature_matmul_int8)
-#define _SIMDINFO_SUPPORTS__ARM_FEATURE_FP16_VECTOR_ARITHMETIC(info)           \
-  ((info).supports_arm_feature_fp16_vector_arithmetic)
-
-#define SIMDINFO_SUPPORTS(info, feature) _SIMDINFO_SUPPORTS##feature(info)
+#define SIMDINFO_SUPPORTS(info, feature) ((info)._supports##feature)
 
 static inline struct simdinfo_t simdinfo_internal() {
   struct simdinfo_t info = {0};
@@ -132,17 +120,16 @@ static inline struct simdinfo_t simdinfo_internal() {
   // source:
   // https://github.com/llvm/llvm-project/blob/50598f0ff44f3a4e75706f8c53f3380fe7faa896/clang/lib/Headers/cpuid.h
 
-  info.supports_avx = (info7.named.ecx & 0x10000000) != 0;
-  info.supports_avx2 = (info7.named.ebx & 0x00000020) != 0;
-  info.supports_f16c = (info1.named.ecx & 0x20000000) != 0;
-  info.supports_fma = (info1.named.ecx & 0x00001000) != 0;
-  info.supports_avxvnni = (info7.named.eax & 0x00000010) != 0;
-  info.supports_avx512f = (info7.named.ebx & 0x00010000) != 0;
-  info.supports_avx512fp16 = (info7.named.edx & 0x00800000) != 0;
-  info.supports_avx512bf16 = (info7.named.edx & 0x00000020) != 0;
-  info.supports_avx512vnni = (info7.named.ecx & 0x00000800) != 0;
-  info.supports_avx512vbmi = (info7.named.ecx & 0x00000002) != 0;
-  info.supports_avx512dq = (info7.named.ebx & 0x00020000) != 0;
+  info._supports__AVX__ = (info7.named.ecx & 0x10000000) != 0;
+  info._supports__AVX2__ = (info7.named.ebx & 0x00000020) != 0;
+  info._supports__F16C__ = (info1.named.ecx & 0x20000000) != 0;
+  info._supports__FMA__ = (info1.named.ecx & 0x00001000) != 0;
+  info._supports__AVXVNNI__ = (info7.named.eax & 0x00000010) != 0;
+  info._supports__AVX512F__ = (info7.named.ebx & 0x00010000) != 0;
+  info._supports__AVX512BF16__ = (info7.named.edx & 0x00000020) != 0;
+  info._supports__AVX512VNNI__ = (info7.named.ecx & 0x00000800) != 0;
+  info._supports__AVX512VBMI__ = (info7.named.ecx & 0x00000002) != 0;
+  info._supports__AVX512DQ__ = (info7.named.ebx & 0x00020000) != 0;
 
   return info;
 
@@ -150,23 +137,23 @@ static inline struct simdinfo_t simdinfo_internal() {
 
 #if defined(__aarch64__)
   // Every 64-bit Arm CPU supports NEON
-  info.supports_neon = 1;
+  info._supports__ARM_NEON = 1;
   // Every arm version > 7 (all aarch64) supports NEON FMA
-  info.supports_neon_fma = 1;
-  info.supports_sve = 0;
-  info.supports_sve2 = 0;
+  info._supports__ARM_FEATURE_FMA = 1;
+  info._supports__ARM_FEATURE_SVE = 0;
+  info._supports__ARM_FEATURE_SVE2 = 0;
 #ifdef __linux__
   unsigned long hwcap = getauxval(AT_HWCAP);
   unsigned long hwcap2 = getauxval(AT_HWCAP2);
   // https://github.com/torvalds/linux/blob/83a7eefedc9b56fe7bfeff13b6c7356688ffa670/arch/arm64/include/uapi/asm/hwcap.h
-  info.supports_sve = (hwcap & HWCAP_SVE) != 0;
-  info.supports_sve2 = (hwcap2 & HWCAP2_SVE2) != 0;
+  info._supports__ARM_FEATURE_SVE = (hwcap & HWCAP_SVE) != 0;
+  info._supports__ARM_FEATURE_SVE2 = (hwcap2 & HWCAP2_SVE2) != 0;
 // This is not always defined for some reason
 #ifndef HWCAP2_I8MM
 #define HWCAP2_I8MM (1 << 13)
 #endif
-  info.supports_arm_feature_matmul_int8 = (hwcap2 & HWCAP2_I8MM) != 0;
-  info.supports_arm_feature_fp16_vector_arithmetic =
+  info._supports__ARM_FEATURE_MATMUL_INT8 = (hwcap2 & HWCAP2_I8MM) != 0;
+  info._supports__ARM_FEATURE_FP16_VECTOR_ARITHMETIC =
       (hwcap & HWCAP_ASIMDHP) != 0;
 #endif // __linux__
 #ifdef __APPLE__
@@ -176,11 +163,12 @@ static inline struct simdinfo_t simdinfo_internal() {
   int hw_optional_arm_feat_i8mm = 0;
   sysctlbyname("hw.optional.arm.FEAT_I8MM", &hw_optional_arm_feat_i8mm, &size,
                NULL, 0);
-  info.supports_arm_feature_matmul_int8 = hw_optional_arm_feat_i8mm;
+  info._supports__ARM_FEATURE_MATMUL_INT8 = hw_optional_arm_feat_i8mm;
   int hw_optional_arm_feat_fp16 = 0;
   sysctlbyname("hw.optional.arm.FEAT_FP16", &hw_optional_arm_feat_fp16, &size,
                NULL, 0);
-  info.supports_arm_feature_fp16_vector_arithmetic = hw_optional_arm_feat_fp16;
+  info._supports__ARM_FEATURE_FP16_VECTOR_ARITHMETIC =
+      hw_optional_arm_feat_fp16;
 #endif // __APPLE__
   return info;
 #endif // __aarch64__
@@ -190,10 +178,10 @@ static inline struct simdinfo_t simdinfo_internal() {
   // https://github.com/torvalds/linux/blob/83a7eefedc9b56fe7bfeff13b6c7356688ffa670/arch/arm/include/uapi/asm/hwcap.h
   unsigned long hwcap = getauxval(AT_HWCAP);
   unsigned long hwcap2 = getauxval(AT_HWCAP2);
-  info.supports_neon = (hwcap & HWCAP_NEON) != 0;
-  info.supports_neon_fma = (hwcap & HWCAP_VFPv4) != 0;
-  info.supports_arm_feature_matmul_int8 = (hwcap2 & HWCAP_I8MM) != 0;
-  info.supports_arm_feature_fp16_vector_arithmetic =
+  info._supports__ARM_NEON = (hwcap & HWCAP_NEON) != 0;
+  info._supports__ARM_FEATURE_FMA = (hwcap & HWCAP_VFPv4) != 0;
+  info._supports__ARM_FEATURE_MATMUL_INT8 = (hwcap2 & HWCAP_I8MM) != 0;
+  info._supports__ARM_FEATURE_FP16_VECTOR_ARITHMETIC =
       (hwcap & HWCAP_ASIMDHP) != 0;
   return info;
 #endif // __linux__
